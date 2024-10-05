226d943b5cc6e6287558866b4bbc03c6b911cb5f
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2024-10-05 18:16:13 +0900

    Box のデモを追加

---
+++ src/pages/basic-demo.tsx
@@ -0,0 +1,54 @@
+import Title from "@/components/ui/title"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef, useState } from "react"
+import { OrbitControls } from "@react-three/drei"
+import { Mesh } from "three"
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+    const ref = useRef<Mesh>(null!)
+
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+
+    useFrame((state, delta) => (ref.current.rotation.x += delta))
+
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+            onPointerOut={(event) => hover(false)}
+        >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+        </mesh>
+    )
+}
+
+export default function BasicDemo() {
+    return (
+        <>
+            <Title>Three.js Test</Title>
+            <Canvas>
+                <ambientLight intensity={Math.PI / 2} />
+                <spotLight
+                    position={[10, 10, 10]}
+                    angle={0.15}
+                    penumbra={1}
+                    decay={0}
+                    intensity={Math.PI}
+                />
+                <pointLight
+                    position={[-10, -10, -10]}
+                    decay={0}
+                    intensity={Math.PI}
+                />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+                <OrbitControls />
+            </Canvas>
+        </>
+    )
+}
